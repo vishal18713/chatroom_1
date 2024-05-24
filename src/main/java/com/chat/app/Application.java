1c6eecec2a84755799ab6da58a048c5ace47b930
vishal18713 <lcs2023052@iiitl.ac.in>
2024-05-24 20:49:31 +0530

    first commit

---
+++ src/main/java/com/chat/app/Application.java
@@ -0,0 +1,13 @@
+package com.chat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	//localhost:9090
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
